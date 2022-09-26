468fef9ebd67b08ccc9a6e596638dfeb046d114d
Nicholas Steinbauer <web@obrien.com>
2022-09-26 15:23:19 -0400

    new edits

---
+++ src/pages/oh-taco/index.md
@@ -0,0 +1,14 @@
+---
+templateKey: oh-taco-page
+path: /oh-taco
+title: Oh-Taco
+featuredimage: /img/hotel-kilbourne-lobby-6-wide.jpg
+---
+Discover Oh Taco, an authentic Mexican restaurant and tequila bar on the first floor of Hotel Kilbourne. With over 75 different tequilas, it is one of the largest selections in Ohio.  With quite the variety to choose from, Oh Taco is known for the "best damn margaritas” including the $100 margarita, El Dorado,  Tequila not your thing? No problemo! We have all your favorite spirits too. 
+
+From house-made Guacamole and Tacos to a gigantic Quesadilla and Burrito, we’ve got your appetite under control. Plenty of Vegetarian and Vegan options as well. Also try our “Not So Authentic Mexican” items such as our Dawgitos, Fiesta Fries, and Wing Wednesdays! 
+
+We have daily specials and seasonal features. Margarita Monday, Taco Tuesday, Wing Wednesday, Thirsty Thursday, TGIF, Brunch on Saturday, and All Day Brunch & $2 Mimosas on Sunday. 
+
+Speaking of seasonal features… you may have heard about our epic Bloody Mary Season. 
+New Year's Day through Super Bowl Sunday, people come from near and far to experience our “Bloody Sunday” creations!  You build it, we make it.  Mark your calendar! 
---
+++ src/templates/oh-taco-page.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+
+export const OhTacoPageTemplate = ({ title, content, contentComponent, featuredimage }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    
+    <>
+    <section id='page-container' className='single-post'>
+     
+      <div className='top-gradient'></div>
+      <div className="full-site-image flex-vertical">
+
+          <div id='hero' className="text-center inside-xl">
+              <h1 className="title">{title}</h1>
+              
+          </div>
+
+          <PreviewCompatibleImage
+          imageInfo={{
+            image: featuredimage,
+            alt: "background",
+          }}
+        />
+
+
+      </div>
+
+    </section>
+
+    <section className='inside-md text-center padding-40 post-content'>
+             
+      <PageContent className="content" content={content} />
+
+    </section>
+
+    </>
+ 
+  )
+}
+
+OhTacoPageTemplate.propTypes = {
+  featuredimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const OhTacoPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <OhTacoPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+        featuredimage={post.frontmatter.featuredimage}
+      />
+    </Layout>
+  )
+}
+
+OhTacoPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default OhTacoPage
+
+export const ohTacoPageQuery = graphql`
+  query OhTacoPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        featuredimage {
+          childImageSharp {
+            fluid(maxWidth: 2000, quality: 60) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
